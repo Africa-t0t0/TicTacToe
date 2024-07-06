bda40e392320034c3da6e3802d806452c6add044
Africa-t0t0 <jorge.ramirezz@mail.udp.cl>
2024-07-05 21:25:44 -0400

    players components

---
+++ src/components/Player.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function Player({initialName, symbol}) {
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
+
+    function handleEditClick() {
+        setIsEditing((editing) => !editing);
+    };
+
+    function handleEditChange(event) {
+        setPlayerName(event.target.value);
+    };
+
+    return (
+        <li>
+            <span className="player">
+                {!isEditing ? <span className="player-name">{playerName}</span>
+                 : <input type="text" required value={playerName} onChange={handleEditChange}/>}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+        </li>
+    );
+}
